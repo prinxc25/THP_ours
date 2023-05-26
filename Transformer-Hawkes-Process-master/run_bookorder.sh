fe8778a4a679ad8492c623184ddeea985e133b18
Priyanka Chudasama <79055915+prinxc25@users.noreply.github.com>
2023-05-26 09:42:08 +0530

    Create run_bookorder.sh

---
+++ Transformer-Hawkes-Process-master/run_bookorder.sh
@@ -0,0 +1,17 @@
+device=0
+data=data/data_bookorder/fold1/
+batch=4
+n_head=4
+n_layers=4
+d_model=128
+d_rnn=64
+d_inner=2048
+d_k=64
+d_v=64
+dropout=0.1
+lr=1e-4
+smooth=0.1
+epoch=100
+log=log.txt
+
+CUDA_DEVICE_ORDER=PCI_BUS_ID CUDA_VISIBLE_DEVICES=$device python Main.py -data $data -batch $batch -n_head $n_head -n_layers $n_layers -d_model $d_model -d_rnn $d_rnn -d_inner $d_inner -d_k $d_k -d_v $d_v -dropout $dropout -lr $lr -smooth $smooth -epoch $epoch -log $log
