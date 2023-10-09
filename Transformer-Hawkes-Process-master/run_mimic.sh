fe998701d3e6ac5240a742bbc9c6105af11b35e3
Priyanka Chudasama <79055915+prinxc25@users.noreply.github.com>
2023-10-09 15:03:30 +0530

    Update run_mimic.sh

--- Transformer-Hawkes-Process-master/run_mimic.sh
+++ Transformer-Hawkes-Process-master/run_mimic.sh
@@ -11,7 +11,7 @@ d_v=16
 dropout=0.1
 lr=1e-4
 smooth=0.1
-epoch=100
+epoch=10
 log=log.txt
 
 CUDA_DEVICE_ORDER=PCI_BUS_ID CUDA_VISIBLE_DEVICES=$device python Main.py -data $data -batch $batch -n_head $n_head -n_layers $n_layers -d_model $d_model -d_rnn $d_rnn -d_inner $d_inner -d_k $d_k -d_v $d_v -dropout $dropout -lr $lr -smooth $smooth -epoch $epoch -log $log
